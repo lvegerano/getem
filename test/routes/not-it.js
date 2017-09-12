8686dd6a9ef4a4234150d57ed1b7c54e22644aca
Luis Vegerano <lvegerano422@gmail.com>
2017-09-12 10:37:05 -0500

    Adds data to test file. Fixes RegEx.

---
+++ test/routes/not-it.js
@@ -0,0 +1,1 @@
+exports.notIt = true;
--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
 const getem = require('..');
-const requireAll = require('require-all');
 
 const {
   experiment,
@@ -13,7 +12,6 @@ const {
 experiment('getem', () => {
 
   test('should get all files with default options', (done) => {
-    // const modules = requireAll('./routes');
     const modules = getem(`${__dirname}/routes`);
     console.log('modules', modules);
     done();
