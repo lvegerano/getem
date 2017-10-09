add7d5ae83885005a3fa5dfbfb4dd8bf5fd0664e
Luis Vegerano <lvegerano422@gmail.com>
2017-10-08 23:14:59 -0500

    Adds tests

--- test/test.js
+++ test/test.js
@@ -11,9 +11,43 @@ const {
 
 experiment('getem', () => {
 
-  test('should get all files with default options', (done) => {
+  test('should not pick up hidden files,folders, or files without the extension .json/.js with default settings', (done) => {
     const modules = getem(`${__dirname}/routes`);
-    console.log('modules', modules);
+
+    expect(modules['hidden.js']).to.be.undefined();
+    expect(modules['.hideme']).to.undefined();
     done();
   });
+
+  test('should filter directories via excludeDir function', (done) => {
+    const modules = getem({
+      dirname: `${__dirname}/routes`,
+      excludeDir(dir) {
+        return dir !== 'sub';
+      },
+    });
+
+    expect(modules.sub).to.be.undefined();
+    expect(modules.coolLib).to.be.a.function();
+    expect(modules['main.js']).to.equal({ theMain: 'yessir' });
+    expect(modules['not-it.js']).to.equal({ notIt: true });
+    done();
+  });
+
+  test('should filter files via filterFile function', (done) => {
+    const modules = getem({
+      dirname: `${__dirname}/routes`,
+      filterFile(file) {
+        return file !== 'not-it.js';
+      },
+    });
+
+    expect(modules.sub).to.be.exist();
+    expect(modules.coolLib).to.be.a.function();
+    expect(modules['main.js']).to.equal({ theMain: 'yessir' });
+    expect(modules['not-it.js']).to.be.undefined();
+    done();
+  });
+
+
 });
